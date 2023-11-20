9deb21aa6a00d73f4b78a2c88faaa0f3661b5e72
williamdenega <williamdenega@gmail.com>
2023-11-20 14:36:10 -0500

    added solution

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,20 @@
 require 'pry'
 
 class String
-
+  
   def sentence?
-    
+    self.include? "."
   end
 
   def question?
-
+    self.include? "?"
   end
 
   def exclamation?
-
+    self.include? "!"
   end
 
   def count_sentences
-
+    self.split(/[!?.]/).reject{ |s| s.nil? || s.empty? || s.strip.empty? }.count
   end
 end
